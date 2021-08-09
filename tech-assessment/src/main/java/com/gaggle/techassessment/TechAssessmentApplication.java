a4816f1f0baf67c47c63aad62acbbca6a19fca5e
WADIGZON DIAZ-WONG <wadigzon@users.noreply.github.com>
2021-08-08 18:01:52 -0700

    added tech-assessment project and folder placeholder

---
+++ tech-assessment/src/main/java/com/gaggle/techassessment/TechAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.gaggle.techassessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechAssessmentApplication.class, args);
+	}
+
+}
