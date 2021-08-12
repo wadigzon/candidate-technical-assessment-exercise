69e85d9bdab7a24089195be4e8fcaf200d5fbff8
WADIGZON DIAZ-WONG <wadigzon@users.noreply.github.com>
2021-08-11 19:55:40 -0700

    reverted back to a working copy, something was broken

--- contacts-api/src/main/java/com/gaggle/techassessment/contactsapi/ContactsApiApplication.java
+++ contacts-api/src/main/java/com/gaggle/techassessment/contactsapi/ContactsApiApplication.java
@@ -1,30 +1,15 @@
 package com.gaggle.techassessment.contactsapi;
 
 
-import java.sql.SQLException;
-
-import org.h2.tools.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 
 @EnableJpaRepositories("com.gaggle.techassessment.contactsapi.repo")
 @EntityScan("com.gaggle.techassessment.contactsapi.model")
 @SpringBootApplication
-@Configuration
 public class ContactsApiApplication {
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-	    return application.sources(ContactsApiApplication.class);
-	}	
-    @Bean(initMethod = "start", destroyMethod = "stop")
-    public Server h2Server() throws SQLException {
-        return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "8084", "-ifNotExists");
-    }	
 	public static void main(String[] args) {
 		SpringApplication.run(ContactsApiApplication.class, args);
 	}
--- contacts-api/src/test/java/com/gaggle/techassessment/contactsapi/ContactsApiApplicationTests.java
+++ contacts-api/src/test/java/com/gaggle/techassessment/contactsapi/ContactsApiApplicationTests.java
@@ -1,92 +1,16 @@
 package com.gaggle.techassessment.contactsapi;
 
-import org.junit.FixMethodOrder;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.RequestBuilder;
-import org.springframework.test.web.servlet.request.*;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.gaggle.techassessment.contactsapi.controller.ContactController;
-import com.gaggle.techassessment.contactsapi.model.Contact;
-import com.gaggle.techassessment.contactsapi.service.ContactService;
-
-import static org.junit.Assert.assertNotNull;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import org.junit.runners.MethodSorters;
-
-@ContextConfiguration(classes= {ContactController.class, ContactService.class})
-@WebMvcTest
-@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+@RunWith(SpringRunner.class)
+@SpringBootTest
 class ContactsApiApplicationTests {
-	@Autowired
-	private MockMvc mockMvc;
-	
-    @MockBean
-    private ContactService contactService;
-    
-    // insert mock contacts
-    @Test
-    public void test1() throws Exception {
-    	Contact mock1 = new Contact();
-    	mock1.setName("Wadigzon Diaz-Wong");
-    	Contact mock2 = new Contact();
-    	mock2.setName("Wadsui Diaz-Alvarado");
-    	Contact mock3 = new Contact();
-    	mock3.setName("Wadsui Diaz-Alvarado");
-        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/api/contacts")
-                .content(asJsonString(mock1))
-                .contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().is(201))
-                .andReturn();    	
-        String resultSS = result.getResponse().getContentAsString();
-        assertNotNull(resultSS);
-        result = mockMvc.perform(MockMvcRequestBuilders.post("/api/contacts")
-                .content(asJsonString(mock2))
-                .contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().is(201))
-                .andReturn();    	
-        resultSS = result.getResponse().getContentAsString();
-        assertNotNull(resultSS);
-        result = mockMvc.perform(MockMvcRequestBuilders.post("/api/contacts")
-                .content(asJsonString(mock3))
-                .contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().is(201))
-                .andReturn();    	
-        resultSS = result.getResponse().getContentAsString();
-        assertNotNull(resultSS);
-    	RequestBuilder requestBuilder = MockMvcRequestBuilders.get(
-				"/api/contacts").accept(
-				MediaType.APPLICATION_JSON);
-    	result = mockMvc.perform(requestBuilder)
-    			.andExpect(status().isOk())
-    			.andReturn();
-    	System.out.println(result.getResponse().getContentAsString());        
-    }
-    
-    // list inserted contacts
-    @Test 
-    public void test2() throws Exception {
-    	RequestBuilder requestBuilder = MockMvcRequestBuilders.get(
-				"/api/contacts").accept(
-				MediaType.APPLICATION_JSON);
-    	MvcResult result = mockMvc.perform(requestBuilder)
-    			.andExpect(status().isOk())
-    			.andReturn();
-    	System.out.println(result.getResponse().getContentAsString());
-    }
-    
-    public static String asJsonString(final Object obj) {
-        try {
-            return new ObjectMapper().writeValueAsString(obj);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
+
+	@Test
+	void contextLoads() {
+	}
+
 }
